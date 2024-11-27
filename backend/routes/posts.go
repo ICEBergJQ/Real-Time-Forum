8470b322b242c8dfaedd9949dff92ec59329e385
ICEBergJQ <salahsk52@gmail.com>
2024-11-27 21:14:12 +0100

    adding author

--- backend/routes/posts.go
+++ backend/routes/posts.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Post struct {
+	Author     string	`json:"author"`
 	ID         int      `json:"id"`
 	Title      string   `json:"title"`
 	Content    string   `json:"content"`
@@ -42,29 +43,29 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodGet {
-        http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-        return
-    }
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
 
-    category := r.URL.Query().Get("category") // Optional filter
-    var filteredPosts []Post
+	category := r.URL.Query().Get("category")
+	var filteredPosts []Post
 
-    mu.Lock()
-    defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-    if category != "" {
-        for _, post := range posts {
-            for _, cat := range post.Categories {
-                if cat == category {
-                    filteredPosts = append(filteredPosts, post)
-                    break
-                }
-            }
-        }
-    } else {
-        filteredPosts = posts
-    }
+	if category != "" {
+		for _, post := range posts {
+			for _, cat := range post.Categories {
+				if cat == category {
+					filteredPosts = append(filteredPosts, post)
+					break
+				}
+			}
+		}
+	} else {
+		filteredPosts = posts
+	}
 
-    json.NewEncoder(w).Encode(filteredPosts)
+	json.NewEncoder(w).Encode(filteredPosts)
 }

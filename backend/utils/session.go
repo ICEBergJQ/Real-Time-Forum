925782e14a69ad12c2632dff92846fe08f16acb4
Eli-Slim <alies120171@gmail.com>
2024-12-31 13:31:27 +0100

    fix logout func delete query & optimize tokencheck func logout user when login from another machine

--- backend/utils/session.go
+++ backend/utils/session.go
@@ -5,25 +5,27 @@ import (
 	"fmt"
 	"net/http"
 
+	"forum/config"
+
 	"github.com/gofrs/uuid"
 )
-func SeesionCreation(user_id int, db *sql.DB) (string ,error){
 
+func SeesionCreation(user_id int, db *sql.DB) (string, error) {
 	token, err := uuid.NewV4()
 	if err != nil {
-		return "error",fmt.Errorf("invalid payload: %w", err)
+		return "error", fmt.Errorf("invalid payload: %w", err)
 	}
 	query := `INSERT INTO session (session_id, user_id, expired_at) 
           VALUES (?, ?, datetime('now', '+1 hour', '+5 days'))`
-    _, err = db.Exec(query, token.String(), user_id)
-    if err != nil {
+	_, err = db.Exec(query, token.String(), user_id)
+	if err != nil {
 		fmt.Println(err)
-        return "error", fmt.Errorf("invalid payload: %w", err)
-    }
-    return token.String(), nil
+		return "error", fmt.Errorf("invalid payload: %w", err)
+	}
+	return token.String(), nil
 }
 
-func TokenCheck(user_id int, r *http.Request, db *sql.DB) bool{
+func TokenCheck(user_id int, r *http.Request, db *sql.DB) bool {
 	cookie, err := r.Cookie("session_token")
 	if err != nil {
 		return false
@@ -33,13 +35,21 @@ func TokenCheck(user_id int, r *http.Request, db *sql.DB) bool{
 	row := db.QueryRow(query, user_id)
 	err = row.Scan(&dbToken)
 	if err != nil {
-		if err == sql.ErrNoRows{
+		if err == sql.ErrNoRows {
 			return false
 		}
 		return true
 	}
-	if cookie.Value == dbToken{
+	if cookie.Value == dbToken {
 		return true
+	} else if cookie.Value != dbToken {
+		query := "DELETE FROM session WHERE session_id = ?"
+		_, err = config.DB.Exec(query, dbToken)
+		if err != nil {
+			// call error func
+			return true
+		}
+		return false
 	}
 	return false
 }

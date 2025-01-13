1c579354b25f9f148774f5007776c9e0c3eef3c3
mohcinedev <mouhcinedev@gmail.com>
2025-01-13 16:19:43 +0100

    list

--- frontend/public/components/registerCmp.js
+++ frontend/public/components/registerCmp.js
@@ -1,33 +1,33 @@
-// export function registerForm() {
-//     return `
-//  <div id="signUpModal" class="modal hidden">
-//         <div class="modal-content">
-//             <span class="close" onclick="closeModal('register')">&times;</span>
-//             <h2 class="log">Sign Up</h2>
-//             <form class="for">
-//                 <label for="username">Full Namzzzzzzzzzzzzze</label>
-//                 <input type="text" id="username" name="username" placeholder="Enter your full name" required>
+export function registerFormp() {
+    return `
+ <div id="signUpModal" class="modal hidden">
+        <div class="modal-content">
+            <span class="close" onclick="closeModal('register')">&times;</span>
+            <h2 class="log">Sign Up</h2>
+            <form class="for">
+                <label for="username">Full Namzzzzzzzzzzzzze</label>
+                <input type="text" id="username" name="username" placeholder="Enter your full name" required>
     
-//                 <label for="email">Email</label>
-//                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required>
     
-//                 <label for="password">Password</label>
-//                 <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
     
-//                 <label for="confirm-password">Confirm Password</label>
-//                 <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" required>
+                <label for="confirm-password">Confirm Password</label>
+                <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" required>
     
-//                 <button type="submit" class="btn_s">Sign Up</button>
-//             </form>
-//             <p class="sign-up-text">
-//                 Already have an account? <a href="#" class="sign-up-link" id="openLogin">Login</a>
-//             </p>
-//         </div>
-//     </div>
-//     `
-// }
+                <button type="submit" class="btn_s">Sign Up</button>
+            </form>
+            <p class="sign-up-text">
+                Already have an account? <a href="#" class="sign-up-link" id="openLogin">Login</a>
+            </p>
+        </div>
+    </div>
+    `
+}
 
-export default function registerForm() {
+export  function registerForm() {
     return  `
     <div id="signUpModal" class="modal hidden">
            <div class="modal-content">
@@ -44,7 +44,7 @@ export default function registerForm() {
                    <input type="password"  id="password" name="password" placeholder="Enter your password" >
        
                    <label for="confirm-password">Confirm Password</label>
-                   <input type="password"  id="confirm-password" name="confirm-password" placeholder="Confirm your password" >
+                   <input type="password"  id="confirm-password" value="111111" name="confirm-password" placeholder="Confirm your password" >
                    <button class="btn_s">Sign Up</button>
                </form>
                <p class="sign-up-text">

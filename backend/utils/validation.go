6f22843fcf5126e2df9c73c3aa0b8233e7ef768f
Eli-Slim <alies120171@gmail.com>
2025-01-14 10:09:13 +0100

    reset to 5eaea9d946242d2a7c59e7cc6aa4e67e2f31b371

--- backend/utils/validation.go
+++ backend/utils/validation.go
@@ -1,8 +1,50 @@
 package utils
 
-import "forum/models"
+import (
+    "errors"
+    "net/mail"
 
-func Validation(user models.User) error{
-	
-	return nil
+	"forum/models"
+)
+
+func containsAll(s string) bool {
+    hasUpper := false
+    hasLower := false
+    hasNumber := false
+    hasSpecial := false
+
+    for _, char := range s {
+        if char >= 'A' && char <= 'Z' {
+            hasUpper = true
+        } else if char >= 'a' && char <= 'z' {
+            hasLower = true
+        } else if char >= '0' && char <= '9' {
+            hasNumber = true
+        } else if (char >= 33 && char <= 47) || (char >= 58 && char <= 64) || 
+			(char >= 91 && char <= 96) || (char >= 123 && char <= 126) {
+            hasSpecial = true
+        }
+    }
+
+    return hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func Validation(user models.User) error {
+    if _, err := mail.ParseAddress(user.Email); err != nil {
+        return errors.New("invalid email address")
+    }
+    if len(user.Password) < 6 {
+        return errors.New("password must be at least 6 characters long")
+    }
+    if !containsAll(user.Password) {
+        return errors.New(`password must contain at least:
+		one uppercase letter,
+		one lowercase letter, 
+		one number, 
+		one printable ASCII special character`)
+    }
+    if user.Username == "" {
+        return errors.New("username cannot be empty")
+    }
+    return nil
 }

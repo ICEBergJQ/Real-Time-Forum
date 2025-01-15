848b10c45e12d7e2ade622216b69caac014d8348
Eli-Slim <alies120171@gmail.com>
2025-01-15 18:14:22 +0100

    minor mods in logger func

--- backend/utils/response_logger.go
+++ backend/utils/response_logger.go
@@ -11,9 +11,9 @@ import (
 func CreateResponseAndLogger(w http.ResponseWriter, statusCode int, err error, message string) {
 	dashes := strings.Repeat("-", 100)
 	if err != nil {
-		log.Printf("Error occurred: %v\nMessage: %s\n%s", err, message, dashes)
+		log.Printf("\nError occurred: %v\nMessage: %s\n%s", err, message, dashes)
 	}else{
-		log.Printf("Success: %s\n%s", message, dashes)
+		log.Printf("\nSuccess: %s\n%s", message, dashes)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -25,6 +25,6 @@ func CreateResponseAndLogger(w http.ResponseWriter, statusCode int, err error, m
 
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(errorResponse); err != nil {
-		log.Printf("Failed to encode error response: %v\n%s", err, dashes)
+		log.Printf("\nFailed to encode error response: %v\n%s", err, dashes)
 	}
 }

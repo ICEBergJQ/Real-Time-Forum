899ce284bc8108f4ca401f8b6cf8b708c64f0967
Eli-Slim <alies120171@gmail.com>
2025-01-17 13:08:39 +0100

    check username if printable && check home path

--- backend/controllers/FileServerController.go
+++ backend/controllers/FileServerController.go
@@ -13,6 +13,10 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request){	
 	// Always serve index.html for non-static requests
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	indexPath := filepath.Join(config.STATIC_DIR, "index.html")
 	http.ServeFile(w, r, indexPath)
 }

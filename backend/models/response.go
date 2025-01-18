5aa51d5e826ffc8d8b4828d93f83240b8fe6685d
sakacmi <salahsks52@outlook.com>
2025-01-18 15:16:01 +0100

    adding few changes

--- backend/models/response.go
+++ backend/models/response.go
@@ -7,10 +7,10 @@ type Response struct {
 type PostResponse struct {
 	Posts          []Post `json:"posts"`
 	Postsremaining int    `json:"postsremaing"`
-	Message        string `json:"message"`
+	Message        string `json:"Message"`
 }
 
 type CommentResponse struct {
 	Comments []Comment `json:"comments"`
-	Message  string    `json:"message"`
+	Message  string    `json:"Message"`
 }
